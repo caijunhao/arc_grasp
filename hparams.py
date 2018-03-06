6e7e9960e2b5bdd9c44cbae499df1f8a7f0b5cc7
Cai Junhao <caijh28@mail2.sysu.edu.cn>
2018-03-06 16:24:07 +0800

    update hyper parameters

--- hparams.py
+++ hparams.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 
 def create_hparams(hparam_string=None):
     hparams = tf.contrib.training.HParams(learning_rate=0.001,
-                                          lr_decay_step=5000,
+                                          lr_decay_step=10000,
                                           lr_decay_rate=0.97,
-                                          batch_size=1,
+                                          lamb=15.0,
+                                          batch_size=8,
                                           image_size=320,
                                           model_name='resnet_v1_101')
     if hparam_string:
