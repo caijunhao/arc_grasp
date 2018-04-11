a04938f4aa4b8b85f4f9d7c6dc51bde0391debb3
Cai Junhao <caijh28@mail2.sysu.edu.cn>
2018-04-11 11:04:01 +0800

    add single tower

--- model.py
+++ model.py
@@ -44,4 +44,40 @@ def model(colors,
             end_points = slim.utils.convert_collection_to_dict(end_points_collection)
     end_points.update(color_end_points)
     end_points.update(depth_end_points)
+    end_points['logits'] = net
     return net, end_points
+
+
+def single_tower(colors,
+                 depths,
+                 num_classes=3,
+                 num_channels=1000,
+                 is_training=True,
+                 global_pool=False,
+                 output_stride=16,
+                 spatial_squeeze=False,
+                 scope='arcnet'):
+    inputs = tf.concat([colors, depths], axis=3)
+    with slim.arg_scope(resnet_v1.resnet_arg_scope()):
+        net, end_points = resnet_v1.resnet_v1_101(inputs=inputs,
+                                                  num_classes=num_channels,
+                                                  is_training=is_training,
+                                                  global_pool=global_pool,
+                                                  output_stride=output_stride,
+                                                  spatial_squeeze=spatial_squeeze,
+                                                  scope=scope+'_tower')
+    with tf.variable_scope(scope, 'arcnet', [net]) as sc:
+        end_points_collection = sc.original_name_scope + '_end_points'
+        # to do: add batch normalization to the following conv layers.
+        with slim.arg_scope([slim.conv2d],
+                            outputs_collections=end_points_collection):
+            net = slim.conv2d(net, 512, [1, 1], scope='conv1')
+            net = slim.conv2d(net, 128, [1, 1], scope='conv2')
+            net = slim.conv2d(net, num_classes, [1, 1], scope='conv3')
+            height, width = net.get_shape().as_list()[1:3]
+            net = tf.image.resize_bilinear(net, [height*2, width*2], name='resize_bilinear')
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+    end_points['logits'] = net
+    return net, end_points
+
+
